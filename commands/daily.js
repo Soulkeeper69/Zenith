4792419e6c270a6ac0e97af09bd0265622c92e12
Nightmare#1234 <brooksroy87@gmail.com>
2018-08-08 04:21:02 -0400

    inital commit

--- commands/daily.js
+++ commands/daily.js
@@ -1,21 +1,32 @@
 const Discord = require(`discord.js`)
 const fs = require(`fs`)
+const mysql = require(`mysql`)
+const poolQuery = require('./../functions/poolQuery');
 exports.run = async (bot, message, args) => {
-    let userdata = JSON.parse(fs.readFileSync(`./storage/userdata.json`, { encoding: 'utf8' }));
-    let userkey = message.author.id + message.guild.id 
-    let dailycheck = ((Date.now() - userdata[userkey].lastdaily) >= 86400000) ? "go" : "nogo"
-    
-    if (userdata[userkey].lastdaily === 0 || dailycheck === "go"){
-        
-        userdata[userkey].money += 500
-        userdata[userkey].lastdaily = Date.now()
-        
-        fs.writeFile(`./storage/userdata.json`, JSON.stringify(userdata), (err) => {
-            message.channel.send (` :tada: You collected your daily all by yourself :laughing:.`) 
-            if(err) console.error(err);  
-        })
+    var userid = message.author.id
+    var messagetime = Date.now()
+    const connect = async function(){
+        poolQuery(`SELECT * FROM econ WHERE userid = '${userid}'`).then(result =>{
+            var userdata = result[0]
+            var timesincedaily = Math.floor(Date.now() - userdata.lastDaily) / 3600000 //hrs
+            var timetilldaily = Math.round(24 - timesincedaily)
+
+            if (timesincedaily < 24){
+                message.channel.send(`You must wait ${timetilldaily} hrs before claiming daily again`)
+            } else {
+                var currentmoney = userdata.money
+                var reward = 500
+                var newmoney = userdata.money
+                
+                poolQuery(`UPDATE econ SET money = '${newmoney}', lastDaily = '${messagetime}' WHERE userid = ${userid} `).then(()=>{
+                    message.channel.send(`You claimed ${reward} souls for your daily reward!`)
+                }).catch(() => {
+                    console.log(`${message.author.username}Daily Failed To Update`)
+                });
+            }
+        }).catch(() => {
+            console.log(`${message.author.username} Daily Profile Couldnt Be Found`)
+        });
     }
-    else if (dailycheck === "nogo") {
-        message.channel.send(`Sorry, You are not able to collect your daily right now!!`)
-    }
-};
+    connect();
+}
