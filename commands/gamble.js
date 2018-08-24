69d812016871905679369c0e3be87123e6a1d245
Soulkeeper <soulkeeper420420@hotmail.com>
2018-08-24 17:13:21 -0400

    changed back to other code

--- commands/gamble.js
+++ commands/gamble.js
@@ -1,29 +1,40 @@
-const Discord = require(`discord.js`);
-const fs = require(`fs`);
+const discord = require(`discord.js`);
 const mysql = require(`mysql`);
 const poolQuery = require(`./../functions/poolQuery`);
 exports.run = async(bot,message,args) => {
     var userid = message.author.id
 
-    if(!args[0])
-    return message.channel.send(`You need to make a bet, you goof!!`)
+    if(!args[0]) return message.channel.send(`You need to make a bet, you goof!!`);
+    poolQuery(`SELECT * FROM econ WHERE userid = '${userid}'`).then(result => {
+        let userdata = result[0];
+        let currentmoney = parseInt(userdata.money)
+        var moneyBet = parseInt(args[0])
 
+        if (isNaN(moneyBet))return message.reply("Bet must be a number, Not a letter!")
 
-
-    const connect = async function(){
-        poolQuery(`SELECT * FROM econ WHERE userid = '${userid}'`).then(result => {
-            let userdata = result[0];
-            let currentmoney = userdata.money
-
-            if (currentmoney < args[0]);
-            return message.channel.send("You dont have enough Souls for that bet")
-
-            message.channel.send('Rolling dice..').then(msg => {
-                var number = Math.round(Math.random() * 100); if(number === 0) number = 1;
-                msg.edit("You rolled " + number)
-            })
-        })
-
-    }
+        if (currentmoney < moneyBet)
+        return message.channel.send("You dont have enough Souls for that bet");
     
+        var number = Math.floor(Math.random() * 100 + 1);
+
+        
+
+        if (number > 70){ 
+            var embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag)
+            .setTitle("You Won!")
+            .setColor(0x00FF00)
+            .setDescription(`You rolled a ${number} and won $${moneyBet}`)
+            message.channel.send(embed)
+            poolQuery(`UPDATE econ SET bal = ${currentmoney + moneyBet} WHERE userid = '${userid}'`)
+        }else{
+            var embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag)
+            .setTitle("You Lost!")
+            .setColor(0xFF0000)
+            .setDescription(`You rolled a ${number} and lost $${moneyBet}`)
+            message.channel.send(embed)
+            poolQuery(`UPDATE econ SET bal = ${currentmoney - moneyBet} WHERE userid = '${userid}'`)
+        } 
+    })
 }
